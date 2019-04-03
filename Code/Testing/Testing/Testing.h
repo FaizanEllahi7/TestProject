105cf241370050c60a15dfbbd65c83545254a72f
Faizan Ellahi <faizanellahi07@gmail.com>
2019-04-03 11:23:32 +0500

    Initial commit.

---
+++ Code/Testing/Testing/Testing.h
@@ -0,0 +1,19 @@
+//
+//  Testing.h
+//  Testing
+//
+//  Created by Faizan Ellahi on 4/3/19.
+//  Copyright © 2019 FaizanEllahi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Testing.
+FOUNDATION_EXPORT double TestingVersionNumber;
+
+//! Project version string for Testing.
+FOUNDATION_EXPORT const unsigned char TestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Testing/PublicHeader.h>
+
+
